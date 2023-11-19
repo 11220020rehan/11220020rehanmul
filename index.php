a538a24151b9d1ee97f9cdfe63f86d1defb90b58
11220020rehan <151423342+11220020rehan@users.noreply.github.com>
2023-11-19 12:57:20 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>11220020 - Rehan Mulyadi - Latihan 7</title>
+    <style>
+        .container {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <table border="1" width='100%' height='100%'cellspacing='0' cellpadding='0'>
+        <tr>
+            <td colspan='2' height='150' align='center'><img src='header1.jpg' width='100%' height='300'></td>
+        </tr>
+        <tr><td valign='top'>
+        <table border='1' width='100%' cellspacing='0' cellpadding='0'>
+		<tr class="container">
+			<th>Menu</a></th>
+            <td width='1700' valign='center' text-align='center' colspan="4" rowspan="4">SELAMAT DATANG</td>
+		</tr>
+		<tr>
+			<td><a href='index.php'>Beranda</td>
+		</tr>
+		<tr>
+			<td><a href='cv.php'>CV</a></td>
+		</tr>
+		<tr>
+			<td><a href='galeri.php'>Galeri</a></td>
+		</tr>
+        </tr>
+	</table>
+	<tr>
+        <td  colspan='2' height='40' align='center'><img src='header2.jpg' width='100%' height='150'></td>
+    </tr>
+    </table>
+</body>
+</html>
