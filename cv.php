e73df662f2898b465b09eb9ff05f32a41fffbe39
11220020rehan <151423342+11220020rehan@users.noreply.github.com>
2023-11-19 12:57:04 +0700

    Add files via upload

---
+++ cv.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+<title>Rehan Mulyadi - 11220020 - Latihan 7</title>
+</head>
+<body>
+<table border='1' width='100%' height='100%' cellspacing='0' cellpadding='0'>
+	<tr>
+		<td colspan='2' height='150' align='center'><img src='header1.jpg' width='100%' height='300'></td>
+	</tr>
+	<tr>
+		<td valign='top'>
+		<table border='1' width='100%' cellspacing='0' cellpadding='0'>
+			<tr>
+				<th>Menu</a></th>
+			</tr>
+			<tr>
+				<td><a href='index.php'>Beranda</td>
+			</tr>
+			<tr>
+				<td><a href='cv.php'>CV</a></td>
+			</tr>
+			<tr>
+				<td><a href='galeri.php'>Galeri</a></td>
+			</tr>
+	</tr></table>
+	<td width='1700' valign='top' align='center'>
+	<img src='cv.jpg' width='70%' height='100%'>
+	</td>
+	<tr>
+        <td  colspan='2' height='40' align='center'><img src='header2.jpg' width='100%' height='150'></td>
+    </tr>
+
+</table>
+</body>
+</html>
---
+++ galeri.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+	<title>Rehan Mulyadi - 11220020 - Latihan 7</title>
+	<style>
+		.gambar-container {
+    		display: flex;
+    		flex-direction: column;
+	}
+
+		.row {
+    		display: flex;
+	}
+
+		.row img {
+    		flex: 1;
+	}
+	</style>
+</head>
+<body>
+<table border='1' width='100%' height='100%' cellspacing='0' cellpadding='0'>
+	<tr>
+		<td colspan='2' height='150' align='center'><img src='header1.jpg' width='100%' height='300'></td>
+	</tr>
+	<tr><td valign='top'>
+	<table border='1' width='100%' cellspacing='0' cellpadding='0'>
+		<tr>
+			<th>Menu</a></th>
+		</tr>
+		<tr>
+			<td><a href='index.php'>Beranda</td>
+		</tr>
+		<tr>
+			<td><a href='cv.php'>CV</a></td>
+		</tr>
+		<tr>
+			<td><a href='galeri.php'>Galeri</a></td>
+		</tr>
+	</tr></table>
+	<td width='1700' valign='top'>
+	<div class="gambar-container">
+    <div class="row">
+        <img src='gambar1.jpg' width='50%' height='75%'>
+        <img src='gambar2.jpg' width='50%' height='75%'>
+    </div>
+    <div class="row">
+        <img src='gambar3.jpg' width='50%' height='75%'>
+        <img src='gambar4.jpg' width='50%' height='75%'>
+    </div>
+</div>
+
+	</td>
+	<tr>
+        <td  colspan='2' height='40' align='center'><img src='header2.jpg' width='100%' height='150'></td>
+    </tr>
+
+
+</table>
+</body>
+</html>
